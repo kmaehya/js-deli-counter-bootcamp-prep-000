b27125e0beee26c415d1c06be0b02b5ffa6c250a
Kevin Choong <kmaehya@gmail.com>
2018-06-14 22:12:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
-  return katzDeliLine.length + 1;
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
