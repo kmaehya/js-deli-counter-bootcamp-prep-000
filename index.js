8ece9794ee3747955012a8ab815b25640046197d
Kevin Choong <kmaehya@gmail.com>
2018-06-14 22:15:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-var katzDeli = [];
+
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing() {
+function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    
+    var first = katzDeliLine.shift();
+    return
+  } else {
+    return "The line is currently empty."
   }
+}
+
+function currentLine(katzDeliLine) {
+  
 }
